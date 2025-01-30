8d1945dcfc431b3169c9c99b9282b55bf1f45a79
Luigi Morel <morelmiles@gmail.com>
2025-01-30 14:44:39 +0300

    add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo chmod 666 /var/run/docker.sock 
+
+echo "Build starting......\n"
+
+echo "Pulling ropes......\n"
+
+git pull 
+
+echo "Build starting......\n"
+
+docker-compose up -d --build
+
+echo "......\n"
+
+echo "......\n"
+
+docker images
+
+docker system prune -a -f 
+
+echo "......\n"
+
+docker ps 
