320508dc46087e59088ce36dc5f847a091c66a7e
Luigi Morel <morelmiles@gmail.com>
2025-01-31 09:27:38 +0300

    chore: switch from nextjs to svelte for web

---
+++ web/src/lib/components/Container.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let className = "";
+</script>
+
+<div class="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="py-8 {className}">
+        <slot />
+    </div>
+</div> 
