bcf2fbaec9119c9e5783fb4b2a492303ffa42057
Luigi Morel <morelmiles@gmail.com>
2025-02-07 07:53:52 +0300

    add vite config

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:5173'
+  }
 });
