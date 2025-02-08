1e755fe7a3936b559ca379e5bd60bcbbd573c4ea
Luigi Morel <morelmiles@gmail.com>
2025-02-08 21:26:07 +0300

    fix: town status tag

--- backend/models/district.go
+++ backend/models/district.go
@@ -7,7 +7,7 @@ import (
 type District struct {
 	Number       string   `gorm:"primaryKey;type:varchar(36);not null;unique" json:"number"`
 	Name         string   `json:"name"`
-	TownStatus   bool     `json:"townstatus"`
+	TownStatus   bool     `json:"town_status"`
 	Counties     []County `gorm:"foreignKey:DistrictNumber;references:Number;constraint: OnUpdate:CASCADE, OnDelete:RESTRICT;" json:"counties,omitempty"`
 	RegionNumber string   `gorm:"type:varchar(36)" json:"region_number"`
 	Region       Region   `json:"region,omitempty" gorm:"foreignKey:RegionNumber;references:Number;constraint: OnUpdate:CASCADE, OnDelete:RESTRICT;"`
